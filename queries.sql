2651724f33d5ef0b288eafa6f7d986af70f5b7d0
ryuzaki <ryuzaki>
2021-07-26 17:18:25 +0530

    folder size query fix

--- queries.sql
+++ queries.sql
@@ -14,7 +14,25 @@ select * from "File" order by "createdAt" desc;
 
 -- c. Find the total size of a folder like total size of files contained in Folder2 which would include size of files File3.jpg and File4.txt
 
-select SUM("size") from "File" where "folderId" in (select "id" from "Folder" where "folderId" = (select "id" from "Folder" where "name" = 'Folder2'));
+select SUM("size") from "File" where "folderId" in (
+WITH RECURSIVE folders AS (
+	SELECT
+		id
+	FROM
+		"Folder"
+	WHERE
+		"folderId" = 1
+	UNION
+		SELECT
+			f.id
+		FROM
+			"Folder" f
+		INNER JOIN folders s ON s.id = f."folderId"
+) SELECT
+	*
+FROM
+	folders
+);
 
 
 -- d. Delete a folder
